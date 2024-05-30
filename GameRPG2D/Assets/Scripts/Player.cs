fa99aef10d5dc3076ea4357df903011686ebd7b1
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-05-29 20:42:59 -0400

    Added Players Run Config

--- GameRPG2D/Assets/Scripts/Player.cs
+++ GameRPG2D/Assets/Scripts/Player.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
 
+    private float initialSpeed;
     private Rigidbody2D rig;
     private Vector2 _direction;
 
@@ -18,15 +20,45 @@ public class Player : MonoBehaviour
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
     }
 
     private void Update()
     {
-        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        OnInput();
+
+        OnRun();
     }
 
     private void FixedUpdate()
     {
+        OnMove();
+    }
+
+    #region Movement
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
         rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
     }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+        }
+    }
+
+    #endregion
 }
